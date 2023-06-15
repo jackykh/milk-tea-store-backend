e58675f89d71e406231b99fee8e7edb0a25506fa
Jacky Cheung <cheungkwingho@gmail.com>
2023-06-15 14:53:09 +0800

    add logger

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import path from "path";
 import express, { Request, Response, NextFunction } from "express";
 import { connect } from "mongoose";
 import dotenv from "dotenv";
+import winston from "winston";
+import expressWinston from "express-winston";
 
 dotenv.config();
 
@@ -35,6 +37,23 @@ app.use((_req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false;
+    }, // optional: allows to skip some log messages based on request and/or response
+  })
+);
 
 app.use("/auth", authRoutes);
 
